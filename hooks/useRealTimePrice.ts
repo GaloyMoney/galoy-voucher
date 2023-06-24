9f9ea5affadf7c3fca2a95110a72c37e4d9d22c1
siddhart1o1 <siddharthtiwarikreplind@gmail.com>
2023-06-24 17:55:13 +0530

    build fix

--- hooks/useRealTimePrice.ts
+++ hooks/useRealTimePrice.ts
@@ -1,63 +1,63 @@
-import { useState, useEffect } from "react";
-import {
-  useRealtimePriceInitialQuery,
-  useRealtimePriceWsSubscription,
-} from "@/utils/generated/graphql";
-import { useDisplayCurrency } from "./useDisplayCurrency";
+// import { useState, useEffect } from "react";
+// import {
+//   useRealtimePriceInitialQuery,
+//   useRealtimePriceWsSubscription,
+// } from "@/utils/generated/graphql";
+// import { useDisplayCurrency } from "./useDisplayCurrency";
 
-interface Currency {
-  __typename: string;
-  id: string;
-  symbol: string;
-  name: string;
-  flag: string;
-  fractionDigits: number;
-}
+// interface Currency {
+//   __typename: string;
+//   id: string;
+//   symbol: string;
+//   name: string;
+//   flag: string;
+//   fractionDigits: number;
+// }
 
-export default function useRealTimePrice(currencyId: string) {
-  const [priceData, setPriceData] = useState(null);
+// export default function useRealTimePrice(currencyId: string) {
+//   const [priceData, setPriceData] = useState(null);
 
-  const {
-    data: initialData,
-    loading,
-    refetch,
-  } = useRealtimePriceInitialQuery({
-    variables: { currency: currencyId },
-    context: {
-      endpoint: "MAINNET",
-    },
-  });
+//   const {
+//     data: initialData,
+//     loading,
+//     refetch,
+//   } = useRealtimePriceInitialQuery({
+//     variables: { currency: currencyId },
+//     context: {
+//       endpoint: "MAINNET",
+//     },
+//   });
 
-  const { data: wsData, loading: wsLoading } = useRealtimePriceWsSubscription({
-    variables: { currency: currencyId },
-    context: {
-      endpoint: "MAINNET",
-    },
-  });
+//   const { data: wsData, loading: wsLoading } = useRealtimePriceWsSubscription({
+//     variables: { currency: currencyId },
+//     context: {
+//       endpoint: "MAINNET",
+//     },
+//   });
 
-  useEffect(() => {
-    if (initialData?.realtimePrice?.btcSatPrice) {
-      setPriceData(initialData?.realtimePrice?.btcSatPrice);
-    }
-  }, [initialData]);
+//   useEffect(() => {
+//     if (initialData?.realtimePrice?.btcSatPrice) {
+//       setPriceData(initialData?.realtimePrice?.btcSatPrice);
+//     }
+//   }, [initialData]);
 
-  useEffect(() => {
-    refetch();
-  }, [wsData]);
+//   useEffect(() => {
+//     refetch();
+//   }, [wsData]);
 
-  const satsToFiat = (sats: number) => {
-    if (!priceData) return null;
-    const { base, offset } = priceData;
-    const priceRef = base / 10 ** offset;
-    return (sats * priceRef).toFixed(2);
-  };
+//   const satsToFiat = (sats: number) => {
+//     if (!priceData) return null;
+//     const { base, offset } = priceData;
+//     const priceRef = base / 10 ** offset;
+//     return (sats * priceRef).toFixed(2);
+//   };
 
-  const fiatToSats = (fiat: number) => {
-    if (!priceData) return null;
-    const { base, offset } = priceData;
-    const priceRef = base / 10 ** offset;
-    return (fiat / priceRef).toFixed();
-  };
+//   const fiatToSats = (fiat: number) => {
+//     if (!priceData) return null;
+//     const { base, offset } = priceData;
+//     const priceRef = base / 10 ** offset;
+//     return (fiat / priceRef).toFixed();
+//   };
 
-  return { priceData, satsToFiat, fiatToSats };
-}
+//   return { priceData, satsToFiat, fiatToSats };
+// }
