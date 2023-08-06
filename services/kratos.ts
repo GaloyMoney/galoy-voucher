ab18bbaabea886b215ec07c18595c2c1ef323da0
siddhart1o1 <siddharthtiwarikreplind@gmail.com>
2023-08-06 13:05:02 +0530

    fix:changes in voucher input and user link not clickable

--- services/kratos.ts
+++ services/kratos.ts
@@ -2,7 +2,7 @@ import { Configuration, FrontendApi } from "@ory/client";
 
 export const ory = new FrontendApi(
   new Configuration({
-    basePath: "http://127.0.0.1:4433",
+    basePath: "http://localhost:4433",
     baseOptions: {
       withCredentials: true,
     },
